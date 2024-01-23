b66a7ab2e04b76bc41eb6f417cd31efad62fa1c3
arthur <arthur.broudoux@gmail.com>
2024-01-23 16:50:41 +0100

    import assets

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,6 @@
 extends Area2D
 
-@export var speed = 400
+@export var speed = 300
 var screen_size
 
 func _ready():
@@ -25,3 +25,13 @@ func _process(delta):
 		
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+
+		
+	if velocity.x != 0:
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+		$AnimatedSprite2D.flip_v = false
+		$AnimatedSprite2D.animation = "left" if velocity.x < 0 else "right"
+	elif velocity.y != 0:
+		$AnimatedSprite2D.flip_v = velocity.y > 0
+		$AnimatedSprite2D.animation = "up" if velocity.y < 0 else "down"
+
